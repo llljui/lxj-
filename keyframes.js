c3106cea932033d454155d697c3b16e29ef60c8c
llljui <872013164@QQ.COM>
2017-01-21 13:29:26 +0800

    Create keyframes.js

---
+++ keyframes.js
@@ -0,0 +1,11 @@
+#arc {width: 400px;height:30px;}
+#arc li:nth-child(1){width: 20px;height:20px;float: left;border-radius: 18px;opacity:1;-webkit-animation:move 0.6s linear 0.6s infinite normal;}
+#arc li:nth-child(2){width: 20px;height:20px;float: left;border-radius: 18px;opacity:1;-webkit-animation:move 0.72s linear 0.72s infinite normal;}
+#arc li:nth-child(3){width: 20px;height:20px;float: left;border-radius: 18px;opacity:1;-webkit-animation:move 0.84s linear 0.84s infinite normal;}
+#arc li:nth-child(4){width: 20px;height:20px;float: left;border-radius: 18px;opacity:1;-webkit-animation:move 0.96s linear 0.96s infinite normal;}
+#arc li:nth-child(5){width: 20px;height:20px;float: left;border-radius: 18px;opacity:1;-webkit-animation:move 1.08s linear 1.08s infinite normal;}
+#arc li:nth-child(6){width: 20px;height:20px;float: left;border-radius: 18px;opacity:1;-webkit-animation:move 1.2s linear 1.2s infinite normal;}
+#arc li:nth-child(7){width: 20px;height:25px;float: left;border-radius: 18px;opacity:1;-webkit-animation:move 1.32s linear 1.32s infinite normal;}
+#arc li:nth-child(8){width: 20px;height:20px;float: left;border-radius: 18px;opacity:1;-webkit-animation:move 1.44s linear 1.44s infinite normal;}
+@-webkit-keyframes move{from {opacity:1;transform: scale(1,1);background:#00ead5;}to {opacity:0;transform: scale(0.3,0.3);background: #ffffff;}}
+#wish{width: 1348px;height: 400px;border: 1px dotted black;}
